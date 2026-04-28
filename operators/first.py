61669cc9ac21efb28d1a17ec034fdf251d8dde1b
varshini0317-max <svarshini074@gmail.com>
2026-04-28 15:03:23 +0530

    Add files via upload

---
+++ operators/first.py
@@ -0,0 +1,81 @@
+'''name="dhanushiya"
+age=17
+height=5.2
+intrest=True
+n=1+3j
+#dataypes
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(intrest))
+print(type(n))
+
+#what is datatypes?
+#    datatypes is 
+name1="manju"
+print(name1)
+
+#arithmetic operator
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=a+b
+print("addition",c)
+print("subtaction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+print("modulus",a%b)
+print("exponentiation",a**b)
+#assignment
+
+#a=10
+#print(a)
+#b=a+5
+#print(b)
+
+
+a=10
+a+=5#a=a+5
+print(a)
+'''
+#relational/conditional/comparison
+age1=21
+age2=18
+print(age1==age2)
+print(age1!=age2)
+print(age1>age2)
+print(age1<age2)
+print(age1<=age2)
+print(age1>=age2)
+
+#logical
+print((age1<age2) and (age1>age2))
+print((age1<age2) or (age1>age2))
+print(not(age1>age2))
+
+# special
+#identity operator is/is not
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z) # is not
+print(x is y)
+print(x == y)
+#membership in/not in
+x = ["apple", "banana"]
+print("banana" in x)
+
+x = ["apple", "banana"]
+print("pineapple"  in x )
+
+
+#bitwise operator
+a=10
+b=5
+print(a&b)
+print(a|b)
+
+
+
+
+
+
